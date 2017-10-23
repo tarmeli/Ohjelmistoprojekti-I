eb39902739fde9f258d848b99f7fd40a9bbe73dc
Tommi Soikkeli <soikkelitommi@gmail.com>
2017-10-23 09:59:34 +0300

    projektin pohja

---
+++ src/main/java/softala/projekti/kysely/KyselyApplication.java
@@ -0,0 +1,12 @@
+package softala.projekti.kysely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyApplication.class, args);
+	}
+}
