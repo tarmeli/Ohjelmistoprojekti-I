be8358dea75d0519ca0d37b0b85ff249c9165f9d
Tommi Soikkeli <soikkelitommi@gmail.com>
2017-11-06 09:46:54 +0200

    lisää kysymyksiä, kyselyn haku id:llä

--- src/main/database/insert_into.sql
+++ src/main/database/insert_into.sql
@@ -2,5 +2,7 @@ INSERT INTO kysely VALUES
 (1, 'Testikysely');
 
 INSERT INTO kysymys VALUES
-(1, 1, 'Mitä kuuluu Marja-Leena?', 'textarea'),
-(2, 1, 'Sumpille?', 'radio');
+(1, 1, 'Mitä kuuluu Marja-Leena?', 'text'),
+(2, 1, 'Sumpille?', 'radio'),
+(3, 1, 'Leipää?', 'range'),
+(4, 1, 'Saako olla lisää olutta?', 'checkbox');
--- src/main/java/softala/projekti/kysely/controller/KyselyController.java
+++ src/main/java/softala/projekti/kysely/controller/KyselyController.java
@@ -1,16 +1,12 @@
 package softala.projekti.kysely.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import softala.projekti.kysely.bean.Kysely;
-import softala.projekti.kysely.bean.Kysymys;
 import softala.projekti.kysely.repository.KyselyRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,7 +19,11 @@ public class KyselyController {
     @RequestMapping(method = RequestMethod.GET)
     public List<Kysely>haeKaikki() {
         return kyselyRepository.findAll();
+    }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public Kysely haeId() {
+        return kyselyRepository.findOne(1);
     }
 
 
