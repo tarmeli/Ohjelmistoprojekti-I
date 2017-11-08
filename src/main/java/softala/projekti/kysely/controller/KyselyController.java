5854e53d90dd1d112b190ebe925350827f6afa48
Tommi Soikkeli <soikkelitommi@gmail.com>
2017-11-08 09:24:54 +0200

    cors pois

--- src/main/java/softala/projekti/kysely/controller/KyselyController.java
+++ src/main/java/softala/projekti/kysely/controller/KyselyController.java
@@ -1,7 +1,6 @@
 package softala.projekti.kysely.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import softala.projekti.kysely.repository.KyselyRepository;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 @RequestMapping(path="/kyselyt")
 public class KyselyController {
 
