ac1fe980adf81b8d7cb2d55f0f403a0503e0d994
Joni Taajamo <JoniMB@Joni-MacBook-Air.local>
2017-11-08 10:12:56 +0200

    testipohja

--- src/test/java/softala/projekti/kysely/KyselyApplicationTests.java
+++ src/test/java/softala/projekti/kysely/KyselyApplicationTests.java
@@ -2,12 +2,32 @@ package softala.projekti.kysely;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import softala.projekti.kysely.repository.KyselyRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT ,
+		classes = KyselyApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource
+@TestPropertySource(
+		locations = "classpath:application-integrationtest.properties"
+)
+
 public class KyselyApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private KyselyRepository repository;
 
 	@Test
 	public void contextLoads() {
