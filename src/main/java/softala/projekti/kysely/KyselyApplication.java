b9eca1fdebfe115f7c1aec147a908c70d3745158
Tommi Soikkeli <soikkelitommi@gmail.com>
2017-11-13 10:51:59 +0200

    deploy ? pls

--- src/main/java/softala/projekti/kysely/KyselyApplication.java
+++ src/main/java/softala/projekti/kysely/KyselyApplication.java
@@ -2,12 +2,19 @@ package softala.projekti.kysely;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KyselyApplication {
+public class KyselyApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
 
         SpringApplication.run(KyselyApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(KyselyApplication.class);
     }
 }
