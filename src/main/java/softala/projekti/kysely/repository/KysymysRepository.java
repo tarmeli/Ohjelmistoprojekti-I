4271aeca7c1ea2296ae03ed61c8583982791615b
jonitaajamo <joni.taajamo@outlook.com>
2017-11-22 10:06:44 +0200

    kommenteja ja testipelleilyä

--- src/main/java/softala/projekti/kysely/repository/KysymysRepository.java
+++ src/main/java/softala/projekti/kysely/repository/KysymysRepository.java
@@ -3,7 +3,8 @@ package softala.projekti.kysely.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softala.projekti.kysely.bean.Kysymys;
-
+// repository vastaa tietokannasta hakemisesta
+// samalla otetaan JpaRepositoryn metodit käyttöön
 @Repository
 public interface KysymysRepository extends JpaRepository<Kysymys, Integer> {
 
--- src/test/java/softala/projekti/kysely/KyselyApplicationTests.java
+++ src/test/java/softala/projekti/kysely/KyselyApplicationTests.java
@@ -1,15 +1,22 @@
 /*
 package softala.projekti.kysely;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+import softala.projekti.kysely.controller.KyselyController;
 import softala.projekti.kysely.repository.KyselyRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
@@ -24,24 +31,26 @@ import softala.projekti.kysely.repository.KyselyRepository;
 //testit tulee tähän
 public class KyselyApplicationTests {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
-	private KyselyRepository repository;
+    @Autowired
+    private KyselyRepository repository;
 
-	@Test
-	public void kyselytTesti()
-		throws Exception{
+    @Autowired
+    private KyselyController controllerToTest;
 
-	}
+    @Before
+    public void setup() {
+        this.mockMvc = standaloneSetup(new KyselyApplication()).build();
+    }
 
-	@Test
-	public void contextLoads()
-        throws Exception{
+    @Test
+    public void kyselyTesti()
+            throws Exception {
 
-	}
+
+    }
 
 }
-
 */
